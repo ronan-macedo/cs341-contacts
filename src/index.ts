13a049454f16053f2a8ecb5d11ca68e91224aaee
Ronan Macedo <rmacedo@enhesa.com>
2024-01-12 01:40:57 +0000

    Feat: Implementing contact.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { initializeDb } from "./database/connection";
+import router from "./routes";
+
+const app = express();
+const port = process.env.PORT ?? 5500;
+
+initializeDb().then(() => {
+    app.use(express.json());
+    app.use('/contact', router);
+
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}).catch((error) => {
+    console.error('Application startup error:', error);
+    process.exit(1);
+});
