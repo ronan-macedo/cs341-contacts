7dc7ef16afa5b6556747ab5a75f338796a1e3945
Ronan Macedo <rmacedo@enhesa.com>
2024-01-12 02:20:37 +0000

    Feat: Add home route.

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,16 @@ const app = express();
 const port = process.env.PORT ?? 5500;
 
 initializeDb().then(() => {
-    app.use(express.json());
-    app.use('/contact', router);
+  app.use(express.json());
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+  app.use('/contact', router);
 
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
 }).catch((error) => {
-    console.error('Application startup error:', error);
-    process.exit(1);
+  console.error('Application startup error:', error);
+  process.exit(1);
 });
