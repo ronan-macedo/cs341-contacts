55a394220fa5bc961707b0dd04a64ed78eedd196
Ronan Macedo <rmacedo@enhesa.com>
2024-01-18 01:13:23 +0000

    Fix: CORS issue.

--- index.js
+++ index.js
@@ -13,10 +13,16 @@ initinitializeDb((error) => {
     console.log(error);
     process.exit(1);
   } else {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    app.use(bodyParser.json());    
-    app.use('/', require('./routes'));
-    
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+      .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();        
+      })
+      .use(bodyParser.json())
+      .use('/', require('./routes'));
+
     app.listen(port, () => {
       console.log('Database connected and HTTP server is running.');
     });
